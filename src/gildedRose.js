ac24367e5117c6380e751d6013611aab48fcbeee
Morgan <64392532+morgan-tw@users.noreply.github.com>
2024-02-19 17:28:09 +0700

    Update the quality of all items of the shop

--- src/gildedRose.js
+++ src/gildedRose.js
@@ -2,6 +2,9 @@ export const createGildedRose = (originalItems) => {
   const items = originalItems;
 
   function UpdateQuality() {
+    items.forEach((item) => {
+      item.updateQuality();
+    });
     return this;
   }
 
--- test/gildedRose.test.js
+++ test/gildedRose.test.js
@@ -1,9 +1,27 @@
-const { createGildedRose } = require("../src/gildedRose");
+import { createGildedRose } from "../src/gildedRose";
 
 describe("Gilded Rose", () => {
-  it("update the quality of all items", () => {
-    const guildedRose = createGildedRose();
-    guildedRose.updateQuality();
-    expect().toHaveBeenCalled(2);
+  describe("update the quality", () => {
+    const createItem = () => {
+      return {
+        updateQuality: jest.fn(),
+      };
+    };
+
+    it("of a single item when there is only one", () => {
+      const anItem = createItem();
+      const guildedRose = createGildedRose([anItem]);
+      guildedRose.updateQuality();
+      expect(anItem.updateQuality).toHaveBeenCalledTimes(1);
+    });
+
+    it("of all items when there are several", () => {
+      const anItem = createItem();
+      const anotherItem = createItem();
+      const guildedRose = createGildedRose([anItem, anotherItem]);
+      guildedRose.updateQuality();
+      expect(anItem.updateQuality).toHaveBeenCalledTimes(1);
+      expect(anotherItem.updateQuality).toHaveBeenCalledTimes(1);
+    });
   });
 });
