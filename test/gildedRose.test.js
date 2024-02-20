8611613963d8428100633c276dcbe4ad6adee116
Morgan <64392532+morgan-tw@users.noreply.github.com>
2024-02-20 13:29:11 +0700

    Sort description blocks

--- test/gildedRose.test.js
+++ test/gildedRose.test.js
@@ -91,115 +91,115 @@ describe("Gilded Rose", () => {
         });
       });
     });
+  });
 
-    describe("for a legendary item", () => {
-      describe("never changes the sellIn over time as it is never pass out", () => {
-        it("like 80 -> 80 -> 80 -> 80", () => {
-          Given()
-            .anItem(aLegendaryItem().getInstance())
-            .then()
-            .itsSellInShouldFollowThisPath("80 -> 80 -> 80 -> 80");
-        });
-
-        it("like -65 -> -65 -> -65 -> -65", () => {
-          Given()
-            .anItem(aLegendaryItem().getInstance())
-            .then()
-            .itsSellInShouldFollowThisPath("-65 -> -65 -> -65 -> -65");
-        });
+  describe("for a legendary item", () => {
+    describe("never changes the sellIn over time as it is never pass out", () => {
+      it("like 80 -> 80 -> 80 -> 80", () => {
+        Given()
+          .anItem(aLegendaryItem().getInstance())
+          .then()
+          .itsSellInShouldFollowThisPath("80 -> 80 -> 80 -> 80");
       });
 
-      describe("never changes the quality over time as it is never age", () => {
-        it("like 80 -> 80 -> 80 -> 80", () => {
-          Given()
-            .anItem(aLegendaryItem().getInstance())
-            .then()
-            .itsQualityShouldFollowThisPath("80 -> 80 -> 80 -> 80");
-        });
-
-        it("like -65 -> -65 -> -65 -> -65", () => {
-          Given()
-            .anItem(aLegendaryItem().getInstance())
-            .then()
-            .itsQualityShouldFollowThisPath("-65 -> -65 -> -65 -> -65");
-        });
+      it("like -65 -> -65 -> -65 -> -65", () => {
+        Given()
+          .anItem(aLegendaryItem().getInstance())
+          .then()
+          .itsSellInShouldFollowThisPath("-65 -> -65 -> -65 -> -65");
       });
     });
 
-    describe("for a backstage pass", () => {
-      describe("decreases the sell in", () => {
-        it(`by one whatever its quality is, like 15 -> 14 -> 13 -> 12 -> 11 -> 10`, () => {
-          Given()
-            .anItem(aBackstagePass().getInstance())
-            .then()
-            .itsSellInShouldFollowThisPath("15 -> 14 -> 13 -> 12 -> 11 -> 10");
-        });
-
-        it(`even bellow zero like 1 -> 0 -> -1 -> -2`, () => {
-          Given()
-            .anItem(aBackstagePass().getInstance())
-            .then()
-            .itsSellInShouldFollowThisPath("1 -> 0 -> -1 -> -2");
-        });
+    describe("never changes the quality over time as it is never age", () => {
+      it("like 80 -> 80 -> 80 -> 80", () => {
+        Given()
+          .anItem(aLegendaryItem().getInstance())
+          .then()
+          .itsQualityShouldFollowThisPath("80 -> 80 -> 80 -> 80");
       });
 
-      describe("decreases the quality", () => {
-        it("by one when the concert is far (more than 10 days ahead), like 25 -> 24 -> 23", () => {
+      it("like -65 -> -65 -> -65 -> -65", () => {
+        Given()
+          .anItem(aLegendaryItem().getInstance())
+          .then()
+          .itsQualityShouldFollowThisPath("-65 -> -65 -> -65 -> -65");
+      });
+    });
+  });
+
+  describe("for a backstage pass", () => {
+    describe("decreases the sell in", () => {
+      it(`by one whatever its quality is, like 15 -> 14 -> 13 -> 12 -> 11 -> 10`, () => {
+        Given()
+          .anItem(aBackstagePass().getInstance())
+          .then()
+          .itsSellInShouldFollowThisPath("15 -> 14 -> 13 -> 12 -> 11 -> 10");
+      });
+
+      it(`even bellow zero like 1 -> 0 -> -1 -> -2`, () => {
+        Given()
+          .anItem(aBackstagePass().getInstance())
+          .then()
+          .itsSellInShouldFollowThisPath("1 -> 0 -> -1 -> -2");
+      });
+    });
+
+    describe("decreases the quality", () => {
+      it("by one when the concert is far (more than 10 days ahead), like 25 -> 24 -> 23", () => {
+        Given()
+          .anItem(aBackstagePass().farFromConcertDate(15).getInstance())
+          .then()
+          .itsQualityShouldFollowThisPath("25 -> 24 -> 23");
+      });
+
+      it("by two as soon as the concert is close, like 25 -> 23 -> 21", () => {
+        Given()
+          .anItem(aBackstagePass().closeToConcertDate(10).getInstance())
+          .then()
+          .itsQualityShouldFollowThisPath("25 -> 23 -> 21");
+      });
+
+      it("by three as soon as the concert is very close, like 25 -> 22 -> 19", () => {
+        Given()
+          .anItem(aBackstagePass().veryCloseToConcertDate(5).getInstance())
+          .then()
+          .itsQualityShouldFollowThisPath("25 -> 22 -> 19");
+      });
+
+      it("down to zero once the concert is passed", () => {
+        Given()
+          .anItem(aBackstagePass().veryCloseToConcertDate(1).getInstance())
+          .then()
+          .itsQualityShouldFollowThisPath("15 -> 12 -> 0");
+      });
+
+      describe("but never bellow 0", () => {
+        it(`when its quality was already 0, like 0 -> 0`, () => {
+          Given()
+            .anItem(aBackstagePass().getInstance())
+            .then()
+            .itsQualityShouldFollowThisPath("0 -> 0");
+        });
+
+        it(`when it is far from the concert date, like 1 -> 0 -> 0`, () => {
           Given()
             .anItem(aBackstagePass().farFromConcertDate(15).getInstance())
             .then()
-            .itsQualityShouldFollowThisPath("25 -> 24 -> 23");
+            .itsQualityShouldFollowThisPath("1 -> 0 -> 0");
         });
 
-        it("by two as soon as the concert is close, like 25 -> 23 -> 21", () => {
+        it(`even when it is close to the concert date, like 1 -> 0 -> 0`, () => {
           Given()
-            .anItem(aBackstagePass().closeToConcertDate(10).getInstance())
+            .anItem(aBackstagePass().closeToConcertDate(9).getInstance())
             .then()
-            .itsQualityShouldFollowThisPath("25 -> 23 -> 21");
+            .itsQualityShouldFollowThisPath("1 -> 0 -> 0");
         });
 
-        it("by three as soon as the concert is very close, like 25 -> 22 -> 19", () => {
+        it(`even when it is very close to the concert date, like 1 -> 0 -> 0`, () => {
           Given()
-            .anItem(aBackstagePass().veryCloseToConcertDate(5).getInstance())
+            .anItem(aBackstagePass().veryCloseToConcertDate(4).getInstance())
             .then()
-            .itsQualityShouldFollowThisPath("25 -> 22 -> 19");
-        });
-
-        it("down to zero once the concert is passed", () => {
-          Given()
-            .anItem(aBackstagePass().veryCloseToConcertDate(1).getInstance())
-            .then()
-            .itsQualityShouldFollowThisPath("15 -> 12 -> 0");
-        });
-
-        describe("but never bellow 0", () => {
-          it(`when its quality was already 0, like 0 -> 0`, () => {
-            Given()
-              .anItem(aBackstagePass().getInstance())
-              .then()
-              .itsQualityShouldFollowThisPath("0 -> 0");
-          });
-
-          it(`when it is far from the concert date, like 1 -> 0 -> 0`, () => {
-            Given()
-              .anItem(aBackstagePass().farFromConcertDate(15).getInstance())
-              .then()
-              .itsQualityShouldFollowThisPath("1 -> 0 -> 0");
-          });
-
-          it(`even when it is close to the concert date, like 1 -> 0 -> 0`, () => {
-            Given()
-              .anItem(aBackstagePass().closeToConcertDate(9).getInstance())
-              .then()
-              .itsQualityShouldFollowThisPath("1 -> 0 -> 0");
-          });
-
-          it(`even when it is very close to the concert date, like 1 -> 0 -> 0`, () => {
-            Given()
-              .anItem(aBackstagePass().veryCloseToConcertDate(4).getInstance())
-              .then()
-              .itsQualityShouldFollowThisPath("1 -> 0 -> 0");
-          });
+            .itsQualityShouldFollowThisPath("1 -> 0 -> 0");
         });
       });
     });
